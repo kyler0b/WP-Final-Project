42136673f9494965ee296188a4e1e90e9db095ce
Kyle Robinson <78045799+kyler0b@users.noreply.github.com>
2022-10-10 02:13:30 -0400

    login/signup/mongo

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
 import Nav from '../components/Nav.vue'
+import router from '../router/index'
+
+
 </script>
 
 <template>
@@ -11,6 +14,11 @@ import Nav from '../components/Nav.vue'
 
     <div class="container">
       <h1 class ="title">Login</h1>
+      <form action="/login" method="POST">
+        <input type="text" name="username" placeholder="username">
+        <input type="password" name="password" placeholder="password">
+        <button href ="/home">login</button>
+      </form>
     </div>
   </div>
 </template>
--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
 import Nav from '../components/Nav.vue'
+import router from '../router/index'
+
 </script>
 
 <template>
@@ -10,7 +12,13 @@ import Nav from '../components/Nav.vue'
     </main>
 
     <div class="container">
-      <h1 class ="title">Sign Up Page</h1>
+      <h1> Sign up form </h1>
+
+      <form action="/signup" method="POST">
+	      <input type="text" name="username" placeholder="username">
+	      <input type="password" name="password" placeholder="password">
+	      <button href="/home">Submit</button>
+      </form>
     </div>
   </div>
 </template>
